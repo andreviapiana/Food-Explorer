50b9ef981b2b0db8364daa1a6cd93241207792bf
André Viapiana <andreviapiana@gmail.com>
2022-11-26 22:59:48 -0300

    :rocket: Pré Deploy Database Complete :rocket:

--- food-explorer-frontend/src/components/Ingredients/index.jsx
+++ food-explorer-frontend/src/components/Ingredients/index.jsx
@@ -2,29 +2,40 @@ import { Container } from "./styles";
 
 import imagePlaceholder from '../../assets/image-not-found-icon.svg';
 
+import aguardente from '../../assets/aguardente.png'
 import alface from '../../assets/alface.png'
 import ameixa from '../../assets/ameixa.png'
 import amendoas from '../../assets/amendoas.png'
 import aniz from '../../assets/Aniz.png'
+import aspargo from '../../assets/aspargo.png'
+import bacon from '../../assets/bacon.png'
+import batata from '../../assets/batata.png'
 import cafe from '../../assets/cafe.png'
 import camarao from '../../assets/camarao.png'
 import canela from '../../assets/canela.png'
 import cebola from '../../assets/cebola.png'
 import claras from '../../assets/claras.png'
+import coca from '../../assets/coca.png'
 import damasco from '../../assets/damasco.png'
 import farinha from '../../assets/farinha.png'
+import filé from '../../assets/filet.png'
 import limao from '../../assets/limao.png'
 import maca from '../../assets/maca.png'
+import manjericao from '../../assets/manjericao.png'
 import maracuja from '../../assets/maracuja.png'
 import massa from '../../assets/massa.png'
+import morango from '../../assets/morango.png'
+import negroni from '../../assets/negroni.png'
 import paoNaan from '../../assets/pao_naan.png'
 import pao from '../../assets/pao.png'
 import pepino from '../../assets/pepino.png'
 import pessego from '../../assets/pessego.png'
 import pesto from '../../assets/pesto.png'
 import presunto from '../../assets/presunto.png'
+import queijo from '../../assets/queijo.png'
 import rabanete from '../../assets/rabanete.png'
 import rucula from '../../assets/rucula.png'
+import sorvete from '../../assets/sorvete.png'
 import tomate from '../../assets/tomate.png'
 import whiskey from '../../assets/whiskey.png'
 
@@ -35,83 +46,116 @@ export function Ingredients({ ingredient }) {
         
         let ingredientImage;
         
-        if (ingredient == "alface") {
-        return ingredientImage = alface
+        if (ingredient == "aguardente") {
+            return ingredientImage = aguardente
+        
+        } else if (ingredient == "alface") {
+            return ingredientImage = alface
+
+        } else if (ingredient == "aspargo") {
+            return ingredientImage = aspargo
         
         } else if (ingredient == "ameixa") {
-        return ingredientImage = ameixa
+            return ingredientImage = ameixa
 
         } else if (ingredient == "amêndoas") {
-        return ingredientImage = amendoas
+            return ingredientImage = amendoas
 
         } else if (ingredient == "aniz") {
-        return ingredientImage = aniz  
+            return ingredientImage = aniz 
+            
+        } else if (ingredient == "bacon") {
+            return ingredientImage = bacon  
+
+        } else if (ingredient == "batata") {
+            return ingredientImage = batata  
 
         } else if (ingredient == "café") {
-        return ingredientImage = cafe
+            return ingredientImage = cafe
 
         } else if (ingredient == "camarão") {
-        return ingredientImage = camarao
+            return ingredientImage = camarao
 
         } else if (ingredient == "canela") {
-        return ingredientImage = canela
+            return ingredientImage = canela
 
         } else if (ingredient == "cebola") {
-        return ingredientImage = cebola
+            return ingredientImage = cebola
         
         } else if (ingredient == "claras") {
-        return ingredientImage = claras 
+            return ingredientImage = claras 
+
+        } else if (ingredient == "coca-cola") {
+            return ingredientImage = coca
 
         } else if (ingredient == "damasco") {
-        return ingredientImage = damasco
+            return ingredientImage = damasco
 
         } else if (ingredient == "farinha") {
-        return ingredientImage = farinha
+            return ingredientImage = farinha
+
+        } else if (ingredient == "filé") {
+            return ingredientImage = filé
 
         } else if (ingredient == "limão") {
-        return ingredientImage = limao
+            return ingredientImage = limao
 
         } else if (ingredient == "maçã") {
-        return ingredientImage = maca
+            return ingredientImage = maca
+
+        } else if (ingredient == "manjericão") {
+            return ingredientImage = manjericao
 
         } else if (ingredient == "maracujá") {
-        return ingredientImage = maracuja
+            return ingredientImage = maracuja
 
         } else if (ingredient == "massa") {
-        return ingredientImage = massa
+            return ingredientImage = massa
+
+        } else if (ingredient == "morango") {
+            return ingredientImage = morango
+
+        } else if (ingredient == "negroni") {
+            return ingredientImage = negroni
 
         } else if (ingredient == "pão") {
-        return ingredientImage = pao
+            return ingredientImage = pao
 
         } else if (ingredient == "pão naan") {
-        return ingredientImage = paoNaan
+            return ingredientImage = paoNaan
 
         } else if (ingredient == "pepino") {
-        return ingredientImage = pepino
+            return ingredientImage = pepino
 
         } else if (ingredient == "pêssego") {
-        return ingredientImage = pessego
+            return ingredientImage = pessego
 
         } else if (ingredient == "pesto") {
-        return ingredientImage = pesto
+            return ingredientImage = pesto
 
         } else if (ingredient == "presunto") {
-        return ingredientImage = presunto
+            return ingredientImage = presunto
+
+        } else if (ingredient == "queijo") {
+            return ingredientImage = queijo  
 
         } else if (ingredient == "rabanete") {
-        return ingredientImage = rabanete
+            return ingredientImage = rabanete
 
         } else if (ingredient == "rucula") {
-        return ingredientImage = rucula
+            return ingredientImage = rucula
 
+        } else if (ingredient == "sorvete") {
+            return ingredientImage = sorvete
+        
         } else if (ingredient == "tomate") {
-        return ingredientImage = tomate
+            return ingredientImage = tomate
 
         } else if (ingredient == "whiskey") {
-        return ingredientImage = whiskey
+            return ingredientImage = whiskey
     
         } else {
-        return ingredientImage = imagePlaceholder
+            return ingredientImage = imagePlaceholder
         }
     }
 
--- food-explorer-frontend/src/components/IngredientsTag/index.jsx
+++ food-explorer-frontend/src/components/IngredientsTag/index.jsx
@@ -14,29 +14,40 @@ export function IngredientsTag({ isNew, value, onClick, ...rest }) {
             />
 
             <datalist id="ingredientName">
+                <option value="Aguardente">Aguardente</option>
                 <option value="Alface">Alface</option>
                 <option value="Ameixa">Ameixa</option>
                 <option value="Amêndoas">Amêndoas</option>
                 <option value="Aniz">Aniz</option>
+                <option value="Aspargo">Aspargo</option>
+                <option value="Batata">Batata</option>
+                <option value="Bacon">Bacon</option>
                 <option value="Café">Café</option>
                 <option value="Camarão">Camarão</option>
                 <option value="Canela">Canela</option>
                 <option value="Cebola">Cebola</option>
                 <option value="Claras">Claras</option>
+                <option value="Coca-Cola">Coca-Cola</option>
                 <option value="Damasco">Damasco</option>
                 <option value="Farinha">Farinha</option>
+                <option value="Filé">Filé</option>
                 <option value="Limão">Limão</option>
                 <option value="Maçã">Maçã</option>
+                <option value="Manjericão">Manjericão</option>
                 <option value="Maracujá">Maracujá</option>
                 <option value="Massa">Massa</option>
+                <option value="Morango">Morango</option>
+                <option value="Negroni">Negroni</option>
                 <option value="Pão Naan">Pão Naan</option>
                 <option value="Pão">Pão</option>
                 <option value="Pepino">Pepino</option>
                 <option value="Pêssego">Pêssego</option>
                 <option value="Pesto">Pesto</option>
                 <option value="Presunto">Presunto</option>
+                <option value="Queijo">Queijo</option>
                 <option value="Rabanete">Rabanete</option>
                 <option value="Rucula">Rucula</option>
+                <option value="Sorvete">Sorvete</option>
                 <option value="Tomate">Tomate</option>
                 <option value="Whiskey">Whiskey</option>
             </datalist>
